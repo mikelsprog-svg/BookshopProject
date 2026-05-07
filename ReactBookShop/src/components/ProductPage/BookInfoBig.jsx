5eff7b98d2fbfe2cf11732e3cad911d985fbcd18
mikelsprog-svg <mikel.s.prog@gmail.com>
2026-05-07 21:25:49 +0200

    Consume hooks de addcarrito en Products y ProductPage

--- ReactBookShop/src/components/ProductPage/BookInfoBig.jsx
+++ ReactBookShop/src/components/ProductPage/BookInfoBig.jsx
@@ -1,8 +1,12 @@
 import {StarGrid} from "../Products/StarGrid.jsx";
 
+import {useCart} from "../../customHooks/useCart.js";
+
 
 export  function BookInfoBig({book})
 {
+    const { addToCart } = useCart();
+
 
     return( <div className="book_info_div">
         <img className="book_image_product_page" src="/placeHolder.jpg" alt="Italian Trulli"/>
@@ -23,7 +27,7 @@ export  function BookInfoBig({book})
         <div className="book_purchase_div">
             <strong className="price">     {book.Price}</strong>
             <strong className="discountPrice"> {book.PriceAfterDiscount}</strong>
-            <button className="book_add_cart"> Add to cart </button>
+            <button onClick={()=>addToCart(book)} className="book_add_cart"> Add to cart </button>
 
         </div>
     </div>)
